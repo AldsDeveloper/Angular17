1fb65a539d4f33a38989abf6861feda4629f1bd2
AldsDeveloper <119023620+AldsDeveloper@users.noreply.github.com>
2024-03-06 20:50:15 +0700

    initial commit

--- src/app/view/exams/exams.component.ts
+++ src/app/view/exams/exams.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-exams',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './exams.component.html',
   styleUrl: './exams.component.css'
 })
 export class ExamsComponent {
-
+  //
 }
