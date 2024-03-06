045b70fce5bd4c043499e64d8ac55ac6968932df
AldsDeveloper <119023620+AldsDeveloper@users.noreply.github.com>
2024-03-06 21:15:47 +0700

    initial commit

--- src/app/view/exams/exams.component.ts
+++ src/app/view/exams/exams.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive,ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Components
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-exams',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent,CommonModule, RouterLink, RouterLinkActive,FormsModule],
   templateUrl: './exams.component.html',
   styleUrl: './exams.component.css'
 })
