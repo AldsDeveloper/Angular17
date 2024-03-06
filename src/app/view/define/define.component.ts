f195a12c3b3714e1be1a70be17aaf8b6b30562f3
AldsDeveloper <119023620+AldsDeveloper@users.noreply.github.com>
2024-03-07 00:52:15 +0700

    initial commit

--- src/app/view/define/define.component.ts
+++ src/app/view/define/define.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit ,NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink, RouterLinkActive, ActivatedRoute,RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive,ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { EditorModule } from '@tinymce/tinymce-angular'
+
 
 
 @Component({
   selector: 'app-define',
   standalone: true,
-  imports: [FormsModule,RouterOutlet,CommonModule],
+  imports: [FormsModule, EditorModule],
   templateUrl: './define.component.html',
+  styleUrls: ['./define.component.css']
+
 })
 export class DefineComponent {
-  
-}
 
+  editorConfig = {
+    height: 300,
+    menubar: true,  
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount',
+      'formatpainter permanentpen pagebreak nonbreaking',
+      'charmap emoticons template',
+    ],
+    toolbar: 'undo redo | formatselect | fontselect fontsizeselect formatpainter | ' +
+             'bold italic underline strikethrough | forecolor backcolor | ' +
+             'alignleft aligncenter alignright alignjustify | ' +
+             'bullist numlist outdent indent | ' +
+             'removeformat | insertfile image media link anchor codesample | ' +
+             'fullscreen preview print | help',
+  };
+
+}
