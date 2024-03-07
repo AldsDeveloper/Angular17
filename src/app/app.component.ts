7f3e336ff3ebee99e96b6b2c2d475dad80e5fe7e
AldsDeveloper <119023620+AldsDeveloper@users.noreply.github.com>
2024-03-08 01:56:18 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,6 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
